44eb7a39da30d64421a43857a7e41474efa1821e
Jackson Calixto <jackson.calixto@gmail.com>
2021-10-02 19:57:08 -0300

    Init

---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+let file = "json.txt";
+//
+$.getData = function(){
+	let dfd = $.Deferred();
+	$.ajax({
+		url:file,
+		type:"GET",
+		dataType:"json",
+		success:function(result){
+			//console.log(result);
+			dfd.resolve(result);
+		},
+		error:function(err){
+			//console.log(err);
+			dfd.reject(err);
+		}
+	});
+	return dfd.promise();
+};
+//
+$.getData().then(
+	function(result){
+		console.log(result);
+	},
+	function(err){
+		console.log(err);
+	}
+)
+//
